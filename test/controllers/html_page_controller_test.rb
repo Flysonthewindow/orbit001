b8f1f1429cedc07cac15004478ee1da97d530e01
David <ilovethejoker@live.co.uk>
2021-09-12 19:11:22 +0200

    Complete HTML-pages

--- test/controllers/html_page_controller_test.rb
+++ test/controllers/html_page_controller_test.rb
@@ -4,10 +4,20 @@ class HtmlPageControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get html_page_home_url
     assert_response :success
+    assert_select "title", "Home | Orbit Feedback Application" 
   end
 
   test "should get help" do
     get html_page_help_url
     assert_response :success
+    assert_select "title", "Help | Orbit Feedback Application" 
+
   end
+
+  test "Should get about" do
+    get html_page_about_url
+    assert_response :success
+    assert_select "title", "About | Orbit Feedback Application" 
+
+  end 
 end
